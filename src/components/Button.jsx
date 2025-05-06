55c392d0826d5c8109ede2304cdf2000b03ec3b7
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-05-06 12:51:02 +0300

    docs: add a comment regarding css translation in the Button presentational component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,6 +6,9 @@ function Button({ icon, iconDescription, onClick = () => {} }) {
 				console.log("I was clicked ! with bubbling or not ?")
 			}}
 		>
+
+      {/* For some reason -translate-1/2 is not working and I don't know why, so
+      I will use the more verbose syntax */}
 			<span className='bg-pink-500/15 absolute top-1/2 left-1/2 transform size-12 -translate-x-1/2 -translate-y-1/2'></span>
 			<img
 				src={icon}
