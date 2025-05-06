58cde81f15fbba527dcbdf10e5643e463a9cff37
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-05-06 14:04:26 +0300

    refactor: use mui components instead of my Button presentational component in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import Button from './Button'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import IconButton from '@mui/material/IconButton';
 function Navbar() {
 	return (
-		<div className='w-full px-10 py-3 flex justify-between align-center'>
+		<div className='flex justify-between w-full px-10 py-3 align-center'>
 			{/* mobile logo */}
 			<div className='block md:hidden'>
 				<img
@@ -12,7 +14,7 @@ function Navbar() {
 			</div>
 
 			{/* desktop logo */}
-			<div className='hidden md:block w-40 h-6 '>
+			<div className='hidden w-40 h-6 md:block '>
 				<img
 					src='/logo.png'
 					alt=''
@@ -20,8 +22,9 @@ function Navbar() {
 			</div>
 
 			{/* cart icon */}
-			<Button icon={'/shopping-bag.svg'} iconDescription={'shopping bag'}/>
-
+			<IconButton onClick={() => console.log("I was clicked yay !")}>
+			<ShoppingCartIcon />
+			</IconButton>
 		</div>
 	)
 }
