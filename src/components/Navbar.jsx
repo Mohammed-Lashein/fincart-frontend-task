7af40a473a62c1414e0e289e9448f03f55c67722
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-05-06 12:07:11 +0300

    refactor: use size-8 instead of w-8 h-8 in the Navbar and prettify the code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,29 @@
-import { CartIcon } from '../icons';
+import { CartIcon } from "../icons"
 function Navbar() {
-  return (
-    <div className='w-full px-10 py-3 flex justify-between align-center'>
-      {/* mobile logo */}
-      <div className='block md:hidden'>
-        <img src="/logo-mobile.png" alt="" className='w-8 h-8'/>
-      </div>
+	return (
+		<div className='w-full px-10 py-3 flex justify-between align-center'>
+			{/* mobile logo */}
+			<div className='block md:hidden'>
+				<img
+					src='/logo-mobile.png'
+					alt=''
+					className='size-8'
+				/>
+			</div>
 
-      {/* desktop logo */}
-      <div className='hidden md:block w-40 h-6 '>
-      <img src="/logo.png" alt="" />
-      </div>
+			{/* desktop logo */}
+			<div className='hidden md:block w-40 h-6 '>
+				<img
+					src='/logo.png'
+					alt=''
+				/>
+			</div>
 
-      {/* cart icon */}
-        <button>
-        <CartIcon/>
-        </button>
-    </div>
-  )
+			{/* cart icon */}
+			<button>
+				<CartIcon />
+			</button>
+		</div>
+	)
 }
 export default Navbar
